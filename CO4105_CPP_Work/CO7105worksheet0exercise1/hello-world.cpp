84ee2328a4c01d0292ad31bde64f2fcba5073c50
ntarannum <59451738+ntarannum@users.noreply.github.com>
2020-06-15 15:01:55 +0100

    Add files via upload

---
+++ CO4105_CPP_Work/CO7105worksheet0exercise1/hello-world.cpp
@@ -0,0 +1,13 @@
+// hello-world.cpp
+// program to print out a "Welcome to C++" message
+// Author:  nt161
+// Version: 1
+
+#include <iostream>
+
+int main () {
+ 
+  std::cout << "Welcome to C++" << std::endl;
+  
+  return 0;
+}
